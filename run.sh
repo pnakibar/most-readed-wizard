5aed0e21ba12aad73ac64784338be451b893c36f
pnakibar <pedronakibar@gmail.com>
2018-11-07 23:05:49 +0100

    added script

---
+++ run.sh
@@ -0,0 +1,5 @@
+node src/main.js https://g1.globo.com/politica
+node src/main.js http://www.folha.uol.com.br/
+node src/main.js https://g1.globo.com/
+node src/main.js https://uol.com.br/
+node src/main.js https://veja.abril.com.br/
--- src/main.js
+++ src/main.js
@@ -9,68 +9,68 @@ const YEAR = 2017
 const URL = process.argv[2]
 
 const rangeOfDate = (initial, final) =>
-    R.until(
-        arr => R.last(arr).isSameOrAfter(final, 'day'),
-        d =>
-            R.append(
-                R.last(d)
-                    .clone()
-                    .add(1, 'day'),
-                d
-            ),
-        [initial.clone()]
-    )
+  R.until(
+    arr => R.last(arr).isSameOrAfter(final, 'day'),
+    d =>
+      R.append(
+        R.last(d)
+          .clone()
+          .add(1, 'day'),
+        d
+      ),
+    [initial.clone()]
+  )
 
 const getFromWayback = (url, date) =>
-    axios.get(
-        `https://archive.org/wayback/available?url=${url}&timestamp=${date}`,
-        { headers: { Accept: 'Application/json' } }
-    )
+  axios.get(
+    `https://archive.org/wayback/available?url=${url}&timestamp=${date}`,
+    { headers: { Accept: 'Application/json' } }
+  )
 
 const getPastHtml = url => axios.get(url)
 
 const saveTo = (url, filename, data) => {
-    const formattedUrl = url.replace(/https:\/\/|http:\/\//, '').replace(/\/|\./g, '-')
-    const filepath = 
+  const formattedUrl = url.replace(/https:\/\/|http:\/\//, '').replace(/\/|\./g, '-')
+  const filepath =
         `/Users/pn/Workspace/tcc/src/htmls/${formattedUrl}`
-    mkdirp.sync(filepath)
-    fs.writeFile(
-        `${filepath}/${filename}.html`,
-        data
-    )
+  mkdirp.sync(filepath)
+  fs.writeFile(
+    `${filepath}/${filename}.html`,
+    data
+  )
 }
 
 async function main() {
-    const url = URL
-    const startDate = moment()
-        .year(YEAR)
-        .startOf('year')
-    const endDate = startDate.clone().endOf('year')
-    const formattedDateRange = rangeOfDate(startDate, endDate).map(x =>
-        x.format('YYYYMMDD000000')
-    )
-    return Promise.each(formattedDateRange, async date => {
-        try {
-            const apiResponse = await getFromWayback(url, date)
-            const {
-                url: waybackURL,
-                timestamp
-            } = apiResponse.data.archived_snapshots.closest
+  const url = URL
+  const startDate = moment()
+    .year(YEAR)
+    .startOf('year')
+  const endDate = moment().clone().month('october').endOf('month')
+  const formattedDateRange = rangeOfDate(startDate, endDate).map(x =>
+    x.format('YYYYMMDD000000')
+  )
+  return Promise.each(formattedDateRange, async date => {
+    try {
+      const apiResponse = await getFromWayback(url, date)
+      const {
+        url: waybackURL,
+        timestamp
+      } = apiResponse.data.archived_snapshots.closest
 
-            console.log(`timestamp: ${timestamp} waybackurl: ${waybackURL}`)
+      console.log(`timestamp: ${timestamp} waybackurl: ${waybackURL}`)
 
-            const { data: pastHtml } = await getPastHtml(waybackURL)
-            return saveTo(url, timestamp, pastHtml)
-        } catch (e) {
-            console.log(
-                `error: timestamp: ${date}`
-            )
-            console.log(e)
-        }
-    })
+      const { data: pastHtml } = await getPastHtml(waybackURL)
+      return saveTo(url, timestamp, pastHtml)
+    } catch (e) {
+      console.log(
+        `error: timestamp: ${date}`
+      )
+      console.log(e)
+    }
+  })
 }
 
 main()
-    .then(() => console.log('endend!'))
-    .then(() => process.exit())
-    .catch(e => console.log(e) && process.exit(1))
+  .then(() => console.log('endend!'))
+  .then(() => process.exit())
+  .catch(e => console.log(e) && process.exit(1))
