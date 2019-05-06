37d2dc9fb4f9514be1177ac9c61759b799950a7c
Pedro Nakibar <pedro.nakibar@klarna.com>
2019-05-06 21:56:50 +0200

    fixed folha scrapper

--- src/parsers/folha.js
+++ src/parsers/folha.js
@@ -1,14 +1,27 @@
 const cheerio = require('cheerio')
 const fs = require('fs')
-const files = fs.readdirSync('./src/htmls/www1-folha-uol-com-br-maispopulares-/')
+const dir = '/Users/pedro.nakibar/Workspace-Pedro/most-readed-wizard/scrapper-results/www1-folha-uol-com-br-maispopulares-'
+const files = fs.readdirSync(dir)
 
 const treated = files.map(filename => {
   const file = fs
     .readFileSync(
-      `/Users/pn/Workspace/tcc/src/htmls/www1-folha-uol-com-br-maispopulares-/${filename}`
+      `${dir}/${filename}`
     )
     .toString()
   const $ = cheerio.load(file)
+  
+  if (filename >= '20180219063227.html') {
+    const mostReaded = Array.from($('#mais-lidas > ol > li > .c-most-popular__content > a')).map(x => ({
+      href: x.attribs.href,
+      title: x.children[0].data
+    }))
+    return {
+      filename,
+      mostReaded
+    }
+  }
+
   const mostReaded = $('section.most_popular > ol > li > a')
     .map(function () {
       const el = $(this)
@@ -22,6 +35,6 @@ const treated = files.map(filename => {
 })
 
 fs.writeFileSync(
-  '/Users/pn/Workspace/tcc/www1-folha-uol-com-br-maispopulares-.json',
+  '/Users/pedro.nakibar/Workspace-Pedro/most-readed-wizard/www1-folha-uol-com-br-maispopulares-.json',
   JSON.stringify(treated, null, 2)
 )
