f59c23b4f2eab2b6eac7dc58f22a34ff4c37b3fe
Pedro Nakibar <pedro.nakibar@klarna.com>
2019-07-27 15:43:20 +0200

    updated notebooks

---
+++ test.js
@@ -0,0 +1,5 @@
+var casper = require('casper').create();
+const page = 'scrapper-results/www-estadao-com-br-mais-lidas/20181106033439.html'
+console.log(casper)
+
+casper.start('/Users/pedro.nakibar/Workspace-Pedro/most-readed-wizard/' + page)
